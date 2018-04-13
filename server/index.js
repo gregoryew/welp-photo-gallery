82d5e1914aef85146182b34d0cf032383c774d25
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-04-13 15:52:23 -0700

    clean the data

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const path = require('path')
-const db = require('../database/index.js')
+const express = require('express');
 
-app.use(express.static(path.join(__dirname, '../client/public')))
-app.use(bodyParser.json())
-app.use((req,res,next)=>{
-    console.log(req.url, req.method)
-    next();
-})
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('../database/index.js');
 
-app.get('/welp/photo/',(req,res)=> {
-    db.getById(5, (error,results) => {
-        if (error) {
-            console.log('error from app.get')
-        } else {
-            res.send(results)
-        }
-    })
-}) 
+app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.get('/api/photo/:id', (req, res) => {
+  db.getById(, (error, results) => {
+    if (error) {
+      console.log('error from app.get');
+    } else {
+      res.send(results);
+    }
+  });
+}); 
 app.listen(3003, () => console.log("I am listening to Dylan's Channel: localhost:3003"));
