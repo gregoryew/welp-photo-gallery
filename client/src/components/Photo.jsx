184ce65b223e129f1639f2337c843a3a0549a0d7
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-04-13 09:47:07 -0700

    edited lightbox according to johnny's code review

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -24,14 +24,14 @@ export default class Photo extends React.Component {
     render() {
         if (this.state.hovered === true) {
             return (
-            <span class = "square" onMouseEnter={() => this.mouseOnPic()} onMouseLeave={() => this.mousLeavePic()}>
-                <a href={`${this.props.url}`} data-lightbox = "food" ><img height="250" width = "250" src={`${this.props.url}`} className="image" /></a>
+            <span class = "square" onMouseEnter={() => this.mouseOnPic()} onMouseLeave={() => this.mouseLeavePic()}>
+                <a href={this.props.url} data-lightbox = "food" ><img height="250" width = "250" src={`${this.props.url}`} className="image" /></a>
             </span>
             ) 
         } else {
             return (
                 <span class = "square" onMouseEnter={() => this.mouseOnPic()} onMouseLeave={() => this.mousLeavePic()}>
-                    <a href={`${this.props.url}`} data-lightbox="food" ><img height="220" width = "220" src={`${this.props.url}`} className="image" /></a>
+                    <a href={this.props.url} data-lightbox="food" ><img height="220" width = "220" src={`${this.props.url}`} className="image" /></a>
                 </span>
             )
         }
