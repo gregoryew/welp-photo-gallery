a544e6f6f106e867737fa6f8776ca800a41e3be5
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-04-13 12:07:50 -0700

    connected server and db

--- database/index.js
+++ database/index.js
@@ -15,3 +15,56 @@
 //     userFollowers: Number,
 //     userReviews: Number,
 // })
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/welp');
+
+var Schema = mongoose.Schema;
+
+var photoSchema = new Schema({
+    photoId: Number,
+    photoUrl: String,
+    id: Number,
+    date: Date,
+    userId: Number,
+    userName: String,
+    userFollowers: Number,
+    userReviews: Number,
+})
+
+var Photo = mongoose.model('Photo', photoSchema);
+
+
+module.exports.getById = (businessId, callback) => {
+    Photo.find({id:businessId}, (error, results) => {
+        if (error) {
+            callback(error, null)
+        } else {
+            callback(null, results)
+        }
+    })
+}
+
+// module.exports.postItem = (params, callback) => {
+//     let query = {
+//         item: params[0],
+//         quantity: params[1]
+//     }
+
+//     let individualItem = new Grocery(query);
+//     individualItem.save((error, results) => {
+//         if (error) {
+//             callback(error, null)
+//         } else {
+//             callback(null, results)
+//         }
+//     })
+// }
+
+// module.exports.getAll(5, (error,results)=> {
+//     if (error) {
+//         console.log('error')
+//     } else {
+//         console.log(results)
+//     }
+// })
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
+const db = require('../database/index.js')
 
 app.use(express.static(path.join(__dirname, '../client/public')))
 app.use(bodyParser.json())
@@ -9,5 +10,16 @@ app.use((req,res,next)=>{
     console.log(req.url, req.method)
     next();
 })
+
+app.get('/welp/photo/',(req,res)=> {
+    db.getById(5, (error,results) => {
+        if (error) {
+            console.log('error from app.get')
+        } else {
+            res.send(results)
+        }
+    })
+}) 
+
 
 app.listen(3003, () => console.log("I am listening to Dylan's Channel: localhost:3003")) 
