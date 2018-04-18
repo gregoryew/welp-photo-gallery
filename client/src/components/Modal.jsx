c61cf9eec3e20f7fc6e9ae6454d6f9d5cdf6807f
Dylan Qian <dylanqian@dylans-mbp.attlocal.net>
2018-04-17 18:29:11 -0700

    finished the commentbox and added to the modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import CommentBox from './CommentBox.jsx';
 
 class Modal extends React.Component {
   render() {
     return (
       <div className="modal" onClick={this.props.handleClose} >
-        <div className="modal-content">
+        <div className="modal-photo">
           <img src={this.props.selectedPhoto.photoUrl} />
+        </div>
+        <div className='modal-comment'>
+          <CommentBox photo={this.props.selectedPhoto} />
         </div>
       </div>
     )
