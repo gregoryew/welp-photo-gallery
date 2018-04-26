89384faa9a1edc38c38ea1a48579bb786cba09c8
Dylan Qian <dylanqian@Dylans-MacBook-Pro.local>
2018-04-25 17:13:58 -0700

    finished implement redis

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/index.js');
+const responseTime = require('response-time');
+
+const redisClient = require('redis').createClient;
+
+const redis = redisClient(6379, 'localhost');
+
 
 app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(bodyParser.json());
@@ -13,14 +19,11 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
+app.use(responseTime());
 
 app.get('/api/photo/:id', (req, res) => {
-  db.getById(req.params.id, (error, results) => {
-    if (error) {
-      console.log('error from app.get');
-    } else {
-      res.json(results);
-    }
+  db.getById(redis, req.params.id, (results) => {
+    res.json(results);
   });
-}); 
+});
 app.listen(3003, () => console.log("I am listening to Dylan's Channel: localhost:3003"));
